ea4ab0c55ea68582c0e3e053244ace06e7c87680
medapatisaiswetha <medapatisaiswetha@missionrnd.com>
2016-03-19 21:41:33 +0530

    Submit Commit

--- src/commonWords.cpp
+++ src/commonWords.cpp
@@ -19,62 +19,55 @@ NOTES: If there are no common words return NULL.
 
 char ** commonWords(char *str1, char *str2)
 {
-	char **c[5];
-	int i = 0, j = 0, k = 0,t=0,count=0;
-	for (i = 0; i < 5; i++)
+	char **c=NULL;
+	int i = 0, j = 0, k = 0,count=0,row=0,l=0,col=0;
+	if (str1 == NULL || str2 == NULL)
+	    return NULL;
+	c = (char**)malloc(0 * sizeof(char*));
+	while (str1[i] != '\0')
 	{
-		*c[i] = (char*)malloc(31 * sizeof(char));
-	}
-	if (str1 == "" || str2 == "" || str1 == NULL || str2 == NULL)
-		return '\0';
-	else
-		for (i = 0; str1[i] != 0; i++)
+		k = i;
+		while (str1[i] == str2[j] && str1[i] != ' ')
 		{
-			for (j = 0; str2[j] != 0; j++)
-			{
-				if (str1[i] != str2[j])
-				{
-					while (str2[j]!= ' '||str2[j]!='\0')
-					{
-						j++;
-					}
-				}
-				else if (str1[i] == str2[j])
-				{
-					t = i;
-					while ((str1[i]!= ' '&&str2[j]!= ' ') || (str1[i]!= '\0'&&str2[j]!= ' ') || (str1[i]!= ' '&&str2[j]!= '\0') || (str1[i]!= '\0'&&str2[j]!= '\0'))
-					{
-						if (str1[i] == str2[j])
-						{
-							fflush(stdin);
-							*c[k][31] = str1[i];
-							i++;
-							j++;
-						}
-						else if (str1[i] != str2[j])
-						{
-							while (str2[j] == ' ' || str2[j] == '\0')
-							{
-								j++;
-							}
-							i = t;
-							k = 0;
-							break;
-						}
-						count++;
-						c[k] = '\0';
-						k++;
-					}
-				}
-				if (str2[j] == '\0')
-				{
-					while (str1[i] == ' ')
-						i++;
-				}
-			}
+			i++;
+			j++;
 		}
+		if((str1[i] == '\0'||str1[i] == ' ') && (str2[j] == ' '||str2[j] == '\0')&&(i!=0)&&(j!=0))
+		{
+						   	count++;
+							c = (char**)realloc(c, count*sizeof(char*));
+							col = 0;
+							c[row] = (char*)malloc(SIZE*sizeof(c));
+							l = k;
+							while (l < i)
+							{
+								c[row][col] = str1[l];
+								l++;
+								col++;
+							}
+						    c[row][col]= '\0';
+							row++;
+							i++;
+							j = 0;
+		}
+		else
+		{
+			while (str2[j] != ' '&& str2[j] != '\0')
+				j++;
+			if (str2[j] == '\0')
+			{
+				j = 0;
+				while (str1[i] != ' '&& str1[i] != '\0')
+					i++;
+				if (str1[i] != '\0')
+					i++;
+			}
+			else
+				j++;
+       }
+	}
 	if (count == 0)
 		return NULL;
 	else
-		return *c;
+		return c;
 }
